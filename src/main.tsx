dc7142e916d7d8b070cae96c324de31d657d3cb1
Guthula Jayasri Sai Nikitha <54980994+jayasrisng@users.noreply.github.com>
2026-04-04 12:09:06 -0500

    chaged app root

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { FlowProvider } from "./context/FlowProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, "")}>
-      <FlowProvider>
-        <App />
-      </FlowProvider>
-    </BrowserRouter>
+    {import.meta.env.PROD ? (
+      <HashRouter>
+        <FlowProvider>
+          <App />
+        </FlowProvider>
+      </HashRouter>
+    ) : (
+      <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, "")}>
+        <FlowProvider>
+          <App />
+        </FlowProvider>
+      </BrowserRouter>
+    )}
   </React.StrictMode>
 );
